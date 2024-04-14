c7852024075a5f1b8fa9e8c29630c8e2591035f0
FCMEXE <semcamargo@gmail.com>
2024-04-14 16:42:32 -0300

    add landindSup and Register

---
+++ Landing-Page/LandingSup/SupScr.js
@@ -0,0 +1,16 @@
+function redirectTolandingPage() {
+  window.location.href = "/Landing-Page/landingPage.html";
+}
+
+function redirectToWhatsApp() {
+  window.location.href =
+    "https://api.whatsapp.com/send?phone=+5515992832003&text=ol%C3%A1%2C+necessito+de+suporte.%0AQuadraConecta%21%21%21%E2%9A%BD%EF%B8%8F";
+}
+
+function redirectToTelefone() {
+  window.location.href = "tel:+15992832003";
+}
+
+function redirectToEmail() {
+  window.location.href = "mailto:lucasjesuss2004@gmail.com";
+}
