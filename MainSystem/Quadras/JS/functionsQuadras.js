46d106ba2cf897a0aa33783d773ba31146fad976
Le0-W-L <leandro.lanzoni0400@gmail.com>
2024-04-22 19:50:22 -0300

    MESMA COISA DA ULTIMA COMMIT

---
+++ MainSystem/Quadras/JS/functionsQuadras.js
@@ -0,0 +1,6 @@
+//FUNÇÕES DA PÁGINA DE QUADRAS
+
+//BOTÃO DE REDIRECIONAR AO SUPORTE
+function redirectToSup() {
+    window.open('Landing-Page/LandingSup/suporte.html', '_blank') //ainda não funciona
+}
