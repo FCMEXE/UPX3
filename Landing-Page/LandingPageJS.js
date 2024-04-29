ba2695d5733a6081522440a84d570264fe247048
FCMEXE <semcamargo@gmail.com>
2024-04-29 11:13:27 -0300

    to ficando maluco - Autentificação feita

---
+++ Landing-Page/LandingPageJS.js
@@ -0,0 +1,89 @@
+
+
+const btn = document.getElementById("btn-form");
+
+btn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const email = document.getElementById("email").value;
+
+  fetch("https://66259991052332d553205bbc.mockapi.io/usuarios")
+    .then((resposta) => resposta.json())
+    .then((dados) => {
+      const usuarioEncontrado = dados.find(
+        (usuario) => usuario.email === email
+      );
+
+      if (usuarioEncontrado) {
+        console.log("foi essa buceta!")
+        window.location.href = "../MainSystem/main.html";
+       
+        
+      } else {
+        // User not found, handle accordingly
+        alert("Faz o Registro ai, Meu Chapa!")
+        
+      }
+    })
+    .catch((error) => {
+       alert("Erro ao encontrar ")
+    });
+});
+
+
+ 
+
+
+  
+
+
+//   //recebe os proprietários da API
+//   const proprietarioResponse = fetch(
+//     "https://66259991052332d553205bbc.mockapi.io/proprietarios"
+//   );
+//   const dadosProprietarios = proprietarioResponse.json();
+
+//   const proprietarioEncontrado = dadosProprietarios.find(
+//     (proprietario) => proprietario.email === email
+//   );
+
+//   //verifica se o usuário foi encontrado
+//   if (usuarioEncontrado) {
+//     if (usuarioEncontrado.senha === senha) {
+//       alert("Login realizado com sucesso!");
+//       window.location.href = "../MainSystem/main.html";
+//     } else {
+//       alert("Senha incorreta!");
+//     }
+//   } else if (proprietarioEncontrado) {
+//     //verifica se o proprietário foi encontrado
+//     if (proprietarioEncontrado.senha === senha) {
+//       alert("Login realizado com sucesso!");
+//       window.location.href = "../MainSystem/main.html";
+//     } else {
+//       alert("Senha incorreta!");
+//     }
+//   } else {
+//     alert("E-mail não cadastrado!");
+//   }
+// });
+
+function scrollToSuporte() {
+  var suporteSection = document.querySelector(".Suporte");
+  suporteSection.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToAboutUs() {
+  var aboutUsSection = document.querySelector(".Sobre-nos");
+  aboutUsSection.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToSignUp() {
+  var aboutUsSection = document.querySelector(".register");
+  aboutUsSection.scrollIntoView({ behavior: "smooth" });
+}
+
+function redirectToIndex() {
+  window.location.href = "./LandingSup/suporte.html";
+}
+
--- MainSystem/Quadras/QuadraCreate/quadrasCreate.js
+++ MainSystem/Quadras/QuadraCreate/quadrasCreate.js
@@ -6,18 +6,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const formData = new FormData(form);
 
-    
     const data = {
       tipoquadra: obterTiposTerrenoSelecionados(),
       numeroplayers: formData.get("numJogadores"),
       endeco: formData.get("endereco"),
-      superficie: "", 
+      superficie: "",
       nomequadra: formData.get("name"),
       precohora: formatarPrecoHora(formData.get("precoPorHora")),
       horariosdisponiveis: obterHorariosDisponiveisSelecionados(),
     };
 
- 
     fetch("https://6626eb22b625bf088c06e78f.mockapi.io/quadras", {
       method: "POST",
       headers: {
@@ -33,33 +31,36 @@ document.addEventListener("DOMContentLoaded", function () {
       })
       .then((responseData) => {
         console.log("Dados enviados com sucesso:", responseData);
-        
       })
       .catch((error) => {
         console.error("Erro ao enviar os dados:", error);
-       
       });
   });
 
- 
   function obterTiposTerrenoSelecionados() {
-    const checkboxes = document.querySelectorAll('input[name="terreno"]:checked');
-    const tiposTerrenoSelecionados = Array.from(checkboxes).map((checkbox) => checkbox.value);
+    const checkboxes = document.querySelectorAll(
+      'input[name="terreno"]:checked'
+    );
+    const tiposTerrenoSelecionados = Array.from(checkboxes).map(
+      (checkbox) => checkbox.value
+    );
     return tiposTerrenoSelecionados;
   }
 
-  
   function formatarPrecoHora(preco) {
-    return "R$ " + parseFloat(preco).toFixed(2); 
+    return "R$ " + parseFloat(preco).toFixed(2);
   }
 
   function obterHorariosDisponiveisSelecionados() {
-    const checkboxes = document.querySelectorAll('input[name="horarios"]:checked');
-    const horariosSelecionados = Array.from(checkboxes).map((checkbox) => checkbox.value);
+    const checkboxes = document.querySelectorAll(
+      'input[name="horarios"]:checked'
+    );
+    const horariosSelecionados = Array.from(checkboxes).map(
+      (checkbox) => checkbox.value
+    );
     return horariosSelecionados;
   }
 });
-
 
 function getInfo() {
   window.location.href = "MainSystem/Quadras/QuadraCreate/quadrasCreate.html";
