12a415bc450c33f65b1b7869a6972bbcc6c4f1d2
Le0-W-L <leandro.lanzoni0400@gmail.com>
2024-05-13 15:40:19 -0300

    ALTERAÇÕES 13/05
    - Adicionado o recurso de pesquisa de quadras.
    
    - Retirado o recurso de fazer LogOut do sistema, pois estava entrando em conflito como a pesquisa das quadras.

--- MainSystem/main.js
+++ MainSystem/main.js
@@ -20,12 +20,20 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch(arquivoHTML)
       .then((response) => {
         if (!response.ok) {
-          throw new Error('Erro ao carregar o arquivo HTML');
+          throw new Error("Erro ao carregar o arquivo HTML");
         }
         return response.text();
       })
       .then((html) => {
-        const secoes = ["home", "quadras", "lobbies", "agenda", "wallet", "proprietario", "logout"];
+        const secoes = [
+          "home",
+          "quadras",
+          "lobbies",
+          "agenda",
+          "wallet",
+          "proprietario",
+          "logout",
+        ];
         secoes.forEach((secao) => {
           document.querySelector(`.${secao}`).innerHTML = html;
         });
@@ -39,7 +47,7 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch(arquivoHome)
       .then((response) => {
         if (!response.ok) {
-          throw new Error('Erro ao carregar o arquivo HTML da Home');
+          throw new Error("Erro ao carregar o arquivo HTML da Home");
         }
         return response.text();
       })
@@ -58,35 +66,44 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+// let emailObj = JSON.parse(emailString);
+// console.log(emailObj.nome);
 
-let emailObj = JSON.parse(emailString);
-console.log(emailObj.nome);
+// function rediricionaNego() {
+//   document.getElementById("yes").addEventListener("click", function () {
+//     localStorage.removeItem("email");
+//     localStorage.removeItem("senha");
+//     window.location.href = "../../Landing-Page/index.html";
+//     alert("Você foi desconectado!");
+//   });
 
-function rediricionaNego() {
-  document.getElementById("yes").addEventListener("click", function () {
-    localStorage.removeItem('email');
-    localStorage.removeItem('senha');
-    window.location.href = "../../Landing-Page/index.html";
-    alert("Você foi desconectado!");
-  });
+//   function redirecionaBranco() {
+//     document.getElementById("cancel").addEventListener("click", function () {
+//       window.history.back();
+//       window.open("../../Home/home.html");
+//     });
+//   }
+// }
 
-  function redirecionaBranco(){
-    document.getElementById("cancel").addEventListener("click", function () {
-      window.history.back();
-       window.open("../../Home/home.html");
-    });
-  }
+//REDIRECIONA O USUARIO PARA A PAGINA DE SUPORTE (BOTAO LOCALIZADO NA PAGINA DE QUADRAS)
+function redirectToSup() {
+  window.open("../Landing-Page/LandingSup/suporte.html");
 }
 
+// FUNÇÃO PARA PESQUISAR AS QUADRAS
+function search () { // criação da ArrowFunction
+  const searchBox = document.getElementById("pesquisa").value.toUpperCase(); //pega as informações que o usuário digitou e transforma em maiúsculo
+  const storeQuadras = document.getElementById("containerCardId");
+  const quadras = document.querySelectorAll(".card");
 
-      
-    
+  quadras.forEach((quadra) => { // roda um loop para todas as quadras e pega o nome das quadras e transforma em maiúsculo
+    const nomeQuadra = quadra.querySelector("h3").textContent.toUpperCase();
 
- 
-
-
-
-
-
-
-
+    if (nomeQuadra.includes(searchBox)) { // verifica se as informações fornecidas pelo usuário batem com os nomes das quadras
+      quadra.style.display = ""; //se sim, mostra as quadras correspondentes
+    } 
+    else {
+      quadra.style.display = "none"; //se não, esconde as quadras não correspondentes
+    }
+  });
+};
